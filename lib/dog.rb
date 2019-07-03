41258511a194b0adab0f153f7af859f3bcb7080c
Conor Owens <conorj.owens@gmail.com>
2019-07-03 03:44:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -52,7 +52,7 @@ class Dog
   end
   
   def self.find_or_create_by(name:, breed:)
-    dog = DB[:conn].excute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed).first
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed).first
     if dog
       dogger = self.new_from_db(dog)
     else
